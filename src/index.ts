dd5fabbcb060e4b035f792a5258b07b120021ceb
Mitchell Mudd <mitchellmudd@outlook.com>
2021-09-25 02:08:59 +0000

    Tried to get the hamburger button to be functional. Not working yet

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 import './styles.css'
+
+
+const hamburgerButton = document.getElementById("hamburgerButton");
+hamburgerButton.onclick = () => {
+    const nav = document.getElementById("nav");
+    console.log(nav)
+    nav.classList.toggle("displayNone");
+};
