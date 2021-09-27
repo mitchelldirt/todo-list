9abf6089a582e8893adbf602ec7b386f469fa2a9
Mitchell Mudd <mitchellmudd@outlook.com>
2021-09-27 13:00:34 +0000

    Completed light mode dark mode functionality

--- src/index.ts
+++ src/index.ts
@@ -13,15 +13,15 @@ const lightDarkMode = document.getElementById("lmdm");
 lightDarkMode.addEventListener('click', (e) => {
     e.preventDefault();
     const color1: string = document.documentElement.style.getPropertyValue('--lightColor');
-    const color2: string = document.documentElement.style.getPropertyValue('--darkColor');
     if (color1 === '#eea47f') {
+        lightDarkMode.textContent = "Dark🌝Mode";
         document.documentElement.style.setProperty('--lightColor', '#00539c')
         document.documentElement.style.setProperty('--darkColor', '#eea47f')
         console.log('1')
     } else {
+        lightDarkMode.textContent = "Light🌞Mode"
         document.documentElement.style.setProperty('--lightColor', '#eea47f')
         document.documentElement.style.setProperty('--darkColor', '#00539c')
         console.log('2')
     }
-
 });
