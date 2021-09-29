73272be338c7146ab3e7a48e24b4ea5ad2895989
Mitchell Mudd <mitchellmudd@outlook.com>
2021-09-29 17:04:10 +0000

    Added modal to add a task. Added animation for hamburger button so it isn't so jarring

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import './styles.css'
-
+import './components/modal.js'
 
 const hamburgerButton = document.getElementById("hamburgerButton");
 hamburgerButton.addEventListener('click', (event) => {
     event.preventDefault();
     let nav = document.getElementById("nav");
-    nav.classList.toggle("displayNone");
+    let mainNav = document.querySelector('.nav-section')
+    //nav.classList.toggle("displayNone");
+    mainNav.classList.toggle('show-nav')
     console.log(nav);
 });
 
