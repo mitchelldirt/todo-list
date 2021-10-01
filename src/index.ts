f2526edce546cd7b91becd149c77cfa5fc1ff611
Mitchell Mudd <mitchellmudd@outlook.com>
2021-10-01 11:07:47 +0000

    Got light mode / dark mode working correctly! Onwards to the logic

--- src/index.ts
+++ src/index.ts
@@ -12,18 +12,16 @@ hamburgerButton.addEventListener('click', (event) => {
 });
 
 const lightDarkMode = document.getElementById("lmdm");
-lightDarkMode.addEventListener('click', (e) => {
-    e.preventDefault();
-    const color1: string = document.documentElement.style.getPropertyValue('--lightColor');
-    if (color1 === '#eea47f') {
-        lightDarkMode.textContent = "Dark🌝Mode";
-        document.documentElement.style.setProperty('--lightColor', '#00539c')
-        document.documentElement.style.setProperty('--darkColor', '#eea47f')
-        console.log('1')
-    } else {
+lightDarkMode.onclick = function LMDM() {
+    let color1: string = document.documentElement.style.getPropertyValue('--lightColor');
+    if (color1 === '#00539c') {
         lightDarkMode.textContent = "Light🌞Mode"
         document.documentElement.style.setProperty('--lightColor', '#eea47f')
         document.documentElement.style.setProperty('--darkColor', '#00539c')
-        console.log('2')
-    }
-});
+    } else {
+        lightDarkMode.textContent = "Dark🌝Mode";
+        document.documentElement.style.setProperty('--lightColor', '#00539c')
+        document.documentElement.style.setProperty('--darkColor', '#eea47f')
+    };
+};
+
