95b3649c6a1ad2b10a5a8d800030bd5e3a4ab453
Mitchell Mudd <mitchellmudd@outlook.com>
2021-10-10 00:09:46 +0000

    Started working on the logic of adding toDoItems. Added a toDoItem type interface. Running into an issue where the stuff isn't display on the screen / has no actual input

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,36 @@ import './styles.css'
 import './components/modal.js'
 import './components/hamburgerButton.js';
 import './components/lightModeDarkMode.js'
+import { toDoItem } from './types';
+
+let inbox: toDoItem[] = [];  
+
+const submitBtn: HTMLElement = document.getElementById('submitBtn');
+submitBtn.onclick = function getInputs(e) {
+e.preventDefault();
+const title: string = document.getElementById("title").textContent;
+console.log('all clear');
+return createObject(title);
+}
+
+function createObject(title: string) {
+let newObject: toDoItem = {
+    title: title
+}
+console.log('all clear');
+return storeObject(newObject);
+}
+
+function storeObject(obj: toDoItem) {
+inbox.push(obj);
+console.log('all clear');
+return displayObjects(inbox);
+}
+
+function displayObjects(array: toDoItem[]) {
+const main: HTMLElement = document.getElementById('content');
+let title: HTMLElement = document.createElement('p');
+title.textContent = array[0].title;
+main.appendChild(title);
+console.log('all clear');
+}
---
+++ src/types.d.ts
@@ -0,0 +1,7 @@
+export interface toDoItem {
+    title: string;
+    descripton?: string;
+    dateTime?: Date;
+    priority?: string;
+    project?: string;
+}
