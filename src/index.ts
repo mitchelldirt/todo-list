b0d38b1a145603ddd01d423fe1ab02c9cd682278
Mitchell Mudd <mitchellmudd@outlook.com>
2021-10-14 16:46:50 +0000

    Fixed ios bug. Also got text to display on the screen

--- src/index.ts
+++ src/index.ts
@@ -4,34 +4,34 @@ import './components/hamburgerButton.js';
 import './components/lightModeDarkMode.js'
 import { toDoItem } from './types';
 
-let inbox: toDoItem[] = [];  
+let inbox: toDoItem[] = [];
 
-const submitBtn: HTMLElement = document.getElementById('submitBtn');
+const submitBtn: HTMLElement = document.getElementById('submitBtn') as HTMLButtonElement;
 submitBtn.onclick = function getInputs(e) {
-e.preventDefault();
-const title: string = document.getElementById("title").textContent;
-console.log('all clear');
-return createObject(title);
+    e.preventDefault();
+    const title = document.getElementById("title") as HTMLInputElement;
+    console.log('all clear');
+    return createObject(title.value);
 }
 
 function createObject(title: string) {
-let newObject: toDoItem = {
-    title: title
-}
-console.log('all clear');
-return storeObject(newObject);
+    let newObject: toDoItem = {
+        title: title
+    }
+    console.log('all clear');
+    return storeObject(newObject);
 }
 
 function storeObject(obj: toDoItem) {
-inbox.push(obj);
-console.log('all clear');
-return displayObjects(inbox);
+    inbox.push(obj);
+    console.log('all clear');
+    return displayObjects(inbox);
 }
 
 function displayObjects(array: toDoItem[]) {
-const main: HTMLElement = document.getElementById('content');
-let title: HTMLElement = document.createElement('p');
-title.textContent = array[0].title;
-main.appendChild(title);
-console.log('all clear');
+    const main: HTMLElement = document.getElementById('content');
+    let title: HTMLElement = document.createElement('p');
+    title.textContent = array[0].title;
+    main.appendChild(title);
+    console.log('all clear');
 }
