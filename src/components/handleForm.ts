5ad1bb86ac7dee52bacc8b5a3d74a9bd235fc07e
Mitchell Mudd <mitchellmudd@outlook.com>
2021-10-27 20:30:21 -0400

    Cleaned up the codebase, restructured folders, started thinking out projects

---
+++ src/components/handleForm.ts
@@ -0,0 +1,82 @@
+import { toDoItem } from '../types';
+import { toggleModal } from '../components/modal';
+import trashBin from '../components/trashBin';
+import unChecked from '../components/unChecked';
+import editButton from '../components/editButton';
+import checked from '../components/checked';
+
+let inbox: toDoItem[] = [];
+let sample: toDoItem[] = [];
+let projects: toDoItem[][] = [inbox, sample];
+
+const submitBtn: HTMLElement = document.getElementById('submitBtn') as HTMLButtonElement;
+submitBtn.onclick = function getInputs(e) {
+    const title = document.getElementById("title") as HTMLInputElement;
+    const description = document.getElementById("description") as HTMLTextAreaElement;
+    const dateTime = document.getElementById("dateTime") as HTMLInputElement;
+    const project = document.getElementById("project") as HTMLSelectElement;
+    if (title.value === "" || dateTime.value === "") {
+        return;
+    }
+    toggleModal();
+    return createObject(title.value, description.value, processDate(dateTime.value), project.value);
+};
+
+function processDate(dateTime: string): string {
+    let timeDateArray: string[] = dateTime.split('T');
+    let time: string = timeDateArray[1];
+    let dateArray: string[] = timeDateArray[0].split('-');
+    let date: string = `${dateArray[1]}/${dateArray[2]}/${dateArray[0].slice(2, 4)}`;
+    return `📅${date}⏰${time}`;
+};
+
+function createObject(title: string, description: string, dateTime: string, project: string) {
+    let newObject: toDoItem = {
+        title: title,
+        description: description,
+        dateTime: dateTime,
+        project: project
+    }
+    return storeObject(newObject);
+};
+
+function storeObject(obj: toDoItem) {
+    // grab the property obj.project once you fix the way this functions
+    inbox.push(obj);
+    return displayObjects(inbox);
+};
+
+function displayObjects(array: toDoItem[]) {
+    const main: HTMLElement = document.getElementById('content');
+    main.innerHTML = ""
+    let counter: number = 1;
+    for (let obj of array) {
+        const id: string = counter.toString();
+        const container: HTMLElement = document.createElement('div') as HTMLDivElement;
+        const dueDate: HTMLElement = document.createElement('p') as HTMLParagraphElement;
+        const title: HTMLElement = document.createElement('p') as HTMLParagraphElement;
+        dueDate.style.color = "black";
+        title.style.color = "black";
+        const checkMarkUnchecked = unChecked();
+        const edit = editButton();
+        const deleteBtn = trashBin();
+        title.innerHTML = obj.title;
+        dueDate.innerHTML = obj.dateTime;
+        container.id = id;
+        container.classList.add("toDoItem");
+        obj.id = id;
+        container.appendChild(checkMarkUnchecked);
+        container.appendChild(title);
+        container.appendChild(dueDate);
+        container.appendChild(edit);
+        container.appendChild(deleteBtn);
+        main.appendChild(container);
+        counter += 1;
+    }
+    resetForm();
+};
+
+function resetForm() {
+    const form = document.getElementById("modalForm") as HTMLFormElement;
+    form.reset();
+};
