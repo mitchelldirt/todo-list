3f8b0efd51061240d814c87393cd61492775227d
mitchelldirt <mitchellmudd@outlook.com>
2021-11-07 09:59:02 -0500

    Projects can be added and you can add tasks into any project and switch between them.

--- src/components/handleProjects.ts
+++ src/components/handleProjects.ts
@@ -5,16 +5,13 @@ import { add } from "date-fns";
 
 let projects: toDoItemArray[] = [];
 
+// Open up the projects page
 const projectBtn = document.getElementById('projects') as HTMLButtonElement;
 projectBtn.onclick = () => {
     const main: HTMLElement = document.getElementById('content');
     main.innerHTML = "";
-    let addProject = document.createElement('button') as HTMLButtonElement;
-    addProject.onclick = () => {
-        let container = document.createElement('div');
-        let input = document.createElement('input') as HTMLInputElement;
-        let submitButton = document.createElement('button') as HTMLButtonElement;
-    }
+    let addProject: HTMLButtonElement = createProjectButton();
+    main.appendChild(addProject);
     for (let project of projects) {
         let element = document.createElement('button');
         element.innerHTML = project.value;
@@ -26,10 +23,10 @@ projectBtn.onclick = () => {
         main.appendChild(element);
     }
     let mainNav: HTMLElement = document.querySelector('.nav-section')
-    //nav.classList.toggle("displayNone");
     mainNav.classList.toggle('show-nav')
 }
 
+//Create a toDoItemArray type object.
 function factoryToDoItemArray(input: string) {
     let array: toDoItem[] = []
     let arrayObj: toDoItemArray = {
@@ -37,11 +34,22 @@ function factoryToDoItemArray(input: string) {
         array: array
     }
     projects.push(arrayObj);
-
+    let selectElement = document.getElementById('project');
+    selectElement.innerHTML = '';
+    let valueCounter: number = 0;
+    for (let project of projects) {
+        let option = document.createElement('option');
+        option.value = valueCounter.toString();
+        option.innerHTML = project.value;
+        selectElement.appendChild(option);
+        valueCounter += 1;
+    }
 };
 
 factoryToDoItemArray('inbox');
 factoryToDoItemArray('sample');
+
+// Used in the handle form section to select which project is needed.
 export function returnProjects() {
     return projects;
 }
@@ -55,4 +63,19 @@ export function changeCurrentProject(projectIndex: string) {
             projectOptions[i].removeAttribute('selected')
         }
     }
+}
+
+function createProjectButton() {
+    let btn = document.createElement('button') as HTMLButtonElement;
+    btn.innerHTML = 'Create Project';
+    btn.onclick = () => {
+        let projectName: string = prompt('Enter the name of your project');
+        if (projectName !== '') {
+            factoryToDoItemArray(projectName);
+            projectBtn.click();
+            let mainNav: HTMLElement = document.querySelector('.nav-section')
+            mainNav.classList.toggle('show-nav')
+        }
+    }
+    return btn;
 }
